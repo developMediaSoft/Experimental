6d098be61c3935c3443ca9380e96f7ead4617774
developMediaSoft <developMediaSoft>
2018-03-26 12:38:09 +0200

    Init

---
+++ Experimental/Experimental/Bindthing/ContainedLabel.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.ComponentModel;
+
+namespace Experimental.Bindthing {
+    public class Model : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName = "") {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string three = "Three";
+        public string Three {
+            get {
+                return three;
+            } set {
+                three = value;
+                NotifyPropertyChanged("Three");
+            }
+        }
+    }
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContainedLabel : ContentView {
+        private Model model = new Model();
+        public static readonly BindableProperty TwoProperty = BindableProperty.Create(nameof(Two), typeof(string), typeof(ContainedLabel), "Two");
+        public string Two { get { return (string)GetValue(TwoProperty); } set { SetValue(TwoProperty, value); model.Three = value; } }
+
+        public ContainedLabel() {
+            InitializeComponent();
+            BindingContext = model;
+        }
+    }
+}
